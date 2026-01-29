c1a6e8893b6147418ca6fb59224fecae64f36c62
Klaudija <klaudijadavidovic0@gmail.com>
2026-01-30 00:17:38 +0100

    Add functionality to calculate average from input

--- Blockwoche 3/Aufgabe11.xamil.cs
+++ Blockwoche 3/Aufgabe11.xamil.cs
@@ -1,1 +1,19 @@
+double summe = 0;
+int anzahl = 0;
+double zahl;
+
+do
+{
+    zahl = double.Parse(txtInput.Text);
+
+    if (zahl != 0)
+    {
+        summe += zahl;
+        anzahl++;
+    }
+}
+while (zahl != 0);
+
+double mittelwert = summe / anzahl;
+txtOutput.Text = "Mittelwert: " + mittelwert;
 
