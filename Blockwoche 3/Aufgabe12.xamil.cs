6d068773839217224d8d03d26ebb33b135325617
Klaudija <klaudijadavidovic0@gmail.com>
2026-01-30 00:17:55 +0100

    Add nested loop to create formatted text output
    Generate a grid of letters and numbers from A1 to H8.

--- Blockwoche 3/Aufgabe12.xamil.cs
+++ Blockwoche 3/Aufgabe12.xamil.cs
@@ -1,1 +1,13 @@
+string text = "";
+
+for (char buchstabe = 'A'; buchstabe <= 'H'; buchstabe++)
+{
+    for (int zahl = 1; zahl <= 8; zahl++)
+    {
+        text += buchstabe + zahl + " ";
+    }
+    text += "\n";
+}
+
+txtOutput.Text = text;
 
