fbf45461d8a684d53ca477a077492dfb4ec9958c
Klaudija <klaudijadavidovic0@gmail.com>
2026-01-30 00:16:30 +0100

    Add nested loops to calculate animal counts

--- Blockwoche 3/Aufgabe8.xamil.cs
+++ Blockwoche 3/Aufgabe8.xamil.cs
@@ -1,1 +1,25 @@
+for (int huehner = 1; huehner <= 50; huehner++)
+{
+    int gaense = huehner * 2;
+
+    for (int schafe = 1; schafe <= 50; schafe++)
+    {
+        int kaninchen = schafe * 3;
+
+        int beine =
+            huehner * 2 +
+            gaense * 2 +
+            schafe * 4 +
+            kaninchen * 4;
+
+        if (beine == 90)
+        {
+            txtOutput.Text =
+                "Hühner: " + huehner + "\n" +
+                "Gänse: " + gaense + "\n" +
+                "Schafe: " + schafe + "\n" +
+                "Kaninchen: " + kaninchen;
+        }
+    }
+}
 
