ac0d9fae40d2bf2c95482cb03ae6dd4e0d4e3e14
Klaudija <klaudijadavidovic0@gmail.com>
2026-01-30 11:20:23 +0100

    Update Aufgabe9.xamil.cs

--- Blockwoche 3/Aufgabe9.xamil.cs
+++ Blockwoche 3/Aufgabe9.xamil.cs
@@ -1,5 +1,49 @@
-double QuaderOberflaeche(double l, double b, double h)
-{
-    return 2 * (l * b + l * h + b * h);
-}
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
+namespace Blockwoche_3
+{
+    /// <summary>
+    /// Interaktionslogik für aufgabe9.xaml
+    /// </summary>
+    public partial class aufgabe9 : Page
+    {
+        public aufgabe9()
+        {
+            InitializeComponent();
+        }
+
+        private void Button9P(object sender, RoutedEventArgs e)
+        {
+            Aufagbe8Frame.Navigate(new aufgabe8()); //leads to last page this case page 1
+        }
+
+        private void Button9N(object sender, RoutedEventArgs e)
+        {
+            Aufagbe8Frame.Navigate(new aufgabe10()); //leads to last page this case page 1
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+                double l = double.Parse(lange.Text);
+                double b = double.Parse(breite.Text);
+                double h = double.Parse(hohe.Text);
+                double ergebniss = 2 * (l * b + l * h + b * h);
+
+                ergebnissTxt.Text = "Ihr Ergebniss ist: " + ergebniss ;  
+
+        }
+    }
+}
