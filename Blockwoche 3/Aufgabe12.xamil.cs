1865d40dc9a77bca3d5e0ff0e9bb576f115262bd
Klaudija <klaudijadavidovic0@gmail.com>
2026-01-30 11:53:26 +0100

    Update Aufgabe12.xamil.cs

--- Blockwoche 3/Aufgabe12.xamil.cs
+++ Blockwoche 3/Aufgabe12.xamil.cs
@@ -1,13 +1,56 @@
-string text = "";
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-for (char buchstabe = 'A'; buchstabe <= 'H'; buchstabe++)
+namespace Blockwoche_3
 {
-    for (int zahl = 1; zahl <= 8; zahl++)
+    /// <summary>
+    /// Interaktionslogik für aufgabe12.xaml
+    /// </summary>
+    public partial class aufgabe12 : Page
     {
-        text += buchstabe + zahl + " ";
+        public aufgabe12()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void Button12P(object sender, RoutedEventArgs e)
+        {
+            Aufagbe12Frame.Navigate(new aufagbe11());
+        }
+
+        private void Button12N(object sender, RoutedEventArgs e)
+        {
+            Aufagbe12Frame.Navigate(new MainWindow());
+        }
+
+        private void BigButton(object sender, RoutedEventArgs e)
+        {
+            string text = "";
+
+            for (char buchstabe = 'A'; buchstabe <= 'H'; buchstabe++)
+            {
+                for (int zahl = 1; zahl <= 8; zahl++)
+                {
+                    text += buchstabe + zahl + " ";
+                }
+                text += "\n";
+            }
+
+            txtOutput.Text = text;
+        }
     }
-    text += "\n";
 }
 
-txtOutput.Text = text;
 
